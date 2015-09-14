929a7664f8d7689f9be4258c77b34880269effb4
lomteankush2 <lomteankush2@gmail.com>
2015-09-14 19:51:45 +0530

    DB for live

--- wp-config.php
+++ wp-config.php
@@ -23,10 +23,10 @@
 define('DB_NAME', 'officefruit');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'ftdev');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', '10gXWOqeaf');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
