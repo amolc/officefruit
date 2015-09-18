f491730782d8ebad4d9be78f147cb539fcd48acc
saurabh.undre <saurabh.undre@fountaintechies.com>
2015-09-18 17:40:54 +0530

    screens added

--- wp-content/themes/sparkling/page-our-basket.php
+++ wp-content/themes/sparkling/page-our-basket.php
@@ -18,77 +18,79 @@ get_header(); ?>
     </main><!-- #main -->
     <div class="container">
     	<div class="row our_basket_section" style="margin: 25px 20px;">
-    		<div class="col-sm-12 col-md-6">
-	    		<div class="col-md-12">
+    		<div class="col-sm-12 col-md-12">
+	    		<div class="col-md-6">
 		    		<img src="http://officefruit.sg/beta/wp-content/uploads/2015/09/how_works_1.png" class="img-responsive oscitas-res-image" alt="">
 		    		<h3 style="text-align: left;">Extra Fruit</h3>
 		    		<p style="text-align: left;">An extra boost of the office’s favorite fruit. 2 kg extra.</p>
 		    	</div>
-		    	<div class="col-md-12">
+		    	<div class="col-md-6 our_basket_sub_section">
+		    		<div class="col-lg-4 col-md-12 col-xs-12 col-sm-12"><p></p>
+						<h4 style="text-align: left;">Bananas</h4>
+						<h3 style="text-align: left;">S$ 8</h3>
+						<h4 style="text-align: left;"></h4>
+					</div>
+					<div class="col-lg-4 col-md-12 col-xs-12 col-sm-12"><p></p>
+						<h4 style="text-align: left;">Apple</h4>
+						<h3 style="text-align: left;">S$ 15</h3>
+						<h4 style="text-align: left;"></h4>
+					</div>
+					<div class="col-lg-4 col-md-12 col-xs-12 col-sm-12"><p></p>
+						<h4 style="text-align: left;">Oranges</h4>
+						<h3 style="text-align: left;">S$ 18</h3>
+						<h4 style="text-align: left;"></h4>
+					</div>
+		    	</div>
+		    </div>	
+		    <div class="col-sm-12 col-md-12">
+	    		<div class="col-md-6">
 		    		<img src="http://officefruit.sg/beta/wp-content/uploads/2015/09/how_works_1.png" class="img-responsive oscitas-res-image" alt="">
 		    		<h3 style="text-align: left;">Nuts</h3>
 		    		<p style="text-align: left;">Make sure there are some healthy snacks around with our popular mixes.</p>
-		    	</div>	
-		    	<div class="col-md-12">
+		    	</div>
+		    	<div class="col-md-6 our_basket_sub_section">
+		    		<div class="col-lg-4 col-md-12 col-xs-12 col-sm-12"><p></p>
+						<h4 style="text-align: left;">Fruits & Nuts mix</h4>
+						<h3 style="text-align: left;">S$ 2</h3>
+						<h4 style="text-align: left;"></h4>
+					</div>
+					<div class="col-lg-4 col-md-12 col-xs-12 col-sm-12"><p></p>
+						<h4 style="text-align: left;">Cashew nuts</h4>
+						<h3 style="text-align: left;">S$ 25</h3>
+						<h4 style="text-align: left;"></h4>
+					</div>
+					<div class="col-lg-4 col-md-12 col-xs-12 col-sm-12"><p></p>
+						<h4 style="text-align: left;">Macadamia</h4>
+						<h3 style="text-align: left;">S$ 25</h3>
+						<h4 style="text-align: left;"></h4>
+					</div>
+		    	</div>
+		    </div>
+		    <div class="col-sm-12 col-md-12">
+	    		<div class="col-md-6">
 		    		<img src="http://officefruit.sg/beta/wp-content/uploads/2015/09/how_works_1.png" class="img-responsive oscitas-res-image" alt="">
 		    		<h3 style="text-align: left;">Flowers</h3>
 		    		<p style="text-align: left;">Give your office a wind of freshness with new flowers every week.</p>
-		    	</div>	
-		  	</div>	
-	    	<div class="col-sm-12 col-md-6">
-	    		<div class="col-sm-12 our_basket_sub_section">
+		    	</div>
+		    	<div class="col-md-6 our_basket_sub_section">
 		    		<div class="col-lg-4 col-md-12 col-xs-12 col-sm-12"><p></p>
-						<h4 style="text-align: left;">4 kg (7-12 people)</h4>
-						<h3 style="text-align: left;">S$ 28</h3>
+						<h4 style="text-align: left;">Corporate</h4>
+						<h3 style="text-align: left;">S$ 25</h3>
 						<h4 style="text-align: left;"></h4>
 					</div>
 					<div class="col-lg-4 col-md-12 col-xs-12 col-sm-12"><p></p>
-						<h4 style="text-align: left;">4 kg (7-12 people)</h4>
-						<h3 style="text-align: left;">S$ 28</h3>
+						<h4 style="text-align: left;">Cuties</h4>
+						<h3 style="text-align: left;">S$ 25</h3>
 						<h4 style="text-align: left;"></h4>
 					</div>
 					<div class="col-lg-4 col-md-12 col-xs-12 col-sm-12"><p></p>
-						<h4 style="text-align: left;">4 kg (7-12 people)</h4>
-						<h3 style="text-align: left;">S$ 28</h3>
+						<h4 style="text-align: left;">Colorful</h4>
+						<h3 style="text-align: left;">S$ 25</h3>
 						<h4 style="text-align: left;"></h4>
 					</div>
-				</div>
-				<div class="col-sm-12 our_basket_sub_section">
-		    		<div class="col-lg-4 col-md-12 col-xs-12 col-sm-12"><p></p>
-						<h4 style="text-align: left;">4 kg (7-12 people)</h4>
-						<h3 style="text-align: left;">S$ 28</h3>
-						<h4 style="text-align: left;"></h4>
-					</div>
-					<div class="col-lg-4 col-md-12 col-xs-12 col-sm-12"><p></p>
-						<h4 style="text-align: left;">4 kg (7-12 people)</h4>
-						<h3 style="text-align: left;">S$ 28</h3>
-						<h4 style="text-align: left;"></h4>
-					</div>
-					<div class="col-lg-4 col-md-12 col-xs-12 col-sm-12"><p></p>
-						<h4 style="text-align: left;">4 kg (7-12 people)</h4>
-						<h3 style="text-align: left;">S$ 28</h3>
-						<h4 style="text-align: left;"></h4>
-					</div>
-				</div>	
-				<div class="col-sm-12 our_basket_sub_section">
-		    		<div class="col-lg-4 col-md-12 col-xs-12 col-sm-12"><p></p>
-						<h4 style="text-align: left;">4 kg (7-12 people)</h4>
-						<h3 style="text-align: left;">S$ 28</h3>
-						<h4 style="text-align: left;"></h4>
-					</div>
-					<div class="col-lg-4 col-md-12 col-xs-12 col-sm-12"><p></p>
-						<h4 style="text-align: left;">4 kg (7-12 people)</h4>
-						<h3 style="text-align: left;">S$ 28</h3>
-						<h4 style="text-align: left;"></h4>
-					</div>
-					<div class="col-lg-4 col-md-12 col-xs-12 col-sm-12"><p></p>
-						<h4 style="text-align: left;">4 kg (7-12 people)</h4>
-						<h3 style="text-align: left;">S$ 28</h3>
-						<h4 style="text-align: left;"></h4>
-					</div>
-				</div>		
-	    	</div>	
-    	</div>
+		    	</div>
+		    </div>	
+		  </div>	
     </div>
   	<form class="form-horizontal" role="form" data-ng-submit="submitOrderForm( orderDetails )">
   	<h1>
