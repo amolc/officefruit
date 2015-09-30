4140250a95f5f3ce33219987fd8c954b3426921d
amol <amolvhankalas@gmail.com>
2015-09-30 19:54:24 +0800

    blank coments templates

--- wp-content/themes/sparkling/content-single.php
+++ wp-content/themes/sparkling/content-single.php
@@ -5,7 +5,7 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?> >
-	
+
 	<div class="post-inner-content">
 		<header class="entry-header page-header">
 
@@ -61,30 +61,5 @@
 		</footer><!-- .entry-meta -->
 	</div>
 
-	<?php if (get_the_author_meta('description')) :  ?>
-		<div class="post-inner-content secondary-content-box">
-      <!-- author bio -->
-      <div class="author-bio content-box-inner">
-
-        <!-- avatar -->
-        <div class="avatar">
-            <?php echo get_avatar(get_the_author_meta('ID') , '60'); ?>
-        </div>
-        <!-- end avatar -->
-
-        <!-- user bio -->
-        <div class="author-bio-content">
-
-          <h4 class="author-name"><a href="<?php echo get_author_posts_url(get_the_author_meta( 'ID' )); ?>"><?php echo get_the_author_meta('display_name'); ?></a></h4>
-         <!--  <p class="author-description">
-              <?php //echo get_the_author_meta('description'); ?>
-          </p>
- -->
-        </div><!-- end .author-bio-content -->
-
-      </div><!-- end .author-bio  -->
-
-		</div>
-		<?php endif; ?>
 
 </article><!-- #post-## -->
